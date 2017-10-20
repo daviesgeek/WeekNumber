9675dcfd562128ccc077b796a3b214bf5a72240d
Matthew Davies <daviesgeek@icloud.com>
2017-10-20 16:40:40 -0700

    Initial Commit

---
+++ WeekNumber/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WeekNumber
+//
+//  Created by Matthew Davies on 10/20/17.
+//  Copyright © 2017 Matthew Davies. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
